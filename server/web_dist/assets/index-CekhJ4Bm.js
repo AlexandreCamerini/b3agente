eb9b6cf29739794f59be27564b1070859120519b
Alexandre Camerini <alexandre.camerini@gmail.com>
2026-08-04 23:16:53 -0300

    chore(v2): publica o web com a camada de opções · carimbo F10-20260804-01

--- server/web_dist/assets/index-CekhJ4Bm.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D-qiRbfo.js","assets/index-7lRcJ-Yk.js"])))=>i.map(i=>d[i]);
-import{r,_ as p}from"./index-7lRcJ-Yk.js";const _=r("AppLauncher",{web:()=>p(()=>import("./web-D-qiRbfo.js"),__vite__mapDeps([0,1])).then(e=>new e.AppLauncherWeb)});export{_ as AppLauncher};
