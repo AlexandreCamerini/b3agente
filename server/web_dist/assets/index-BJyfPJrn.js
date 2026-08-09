db9a491fce171c25efddc00946356ac4f5434dcf
Alexandre Camerini <alexandre.camerini@gmail.com>
2026-08-09 16:04:37 -0300

    Corrige orçamento voltando a 10000 no iOS: sync device→servidor de initialBudget (F10-20260809-05)

---
+++ server/web_dist/assets/index-BJyfPJrn.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CPGz2xGL.js","assets/index-DixozlWM.js"])))=>i.map(i=>d[i]);
+import{r,_ as p}from"./index-DixozlWM.js";const _=r("AppLauncher",{web:()=>p(()=>import("./web-CPGz2xGL.js"),__vite__mapDeps([0,1])).then(e=>new e.AppLauncherWeb)});export{_ as AppLauncher};
---
+++ server/web_dist/assets/index-HTSX2U6f.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-FrjIFMWK.js","assets/index-DixozlWM.js"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-DixozlWM.js";const _=p("App",{web:()=>r(()=>import("./web-FrjIFMWK.js"),__vite__mapDeps([0,1])).then(e=>new e.AppWeb)});export{_ as App};
