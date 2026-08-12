585039376e2399c9c8ceb617a1cb780f1f7bff42
Alexandre Camerini <alexandre.camerini@gmail.com>
2026-08-12 00:37:30 -0300

    qa/45 Decisão 1: Perfil em 5 telas + Fonte de dados parametrizável (intervalo do spot)

---
+++ server/web_dist/assets/index-CUloKHqA.js
@@ -0,0 +1,1 @@
+import{r as i}from"./index-Hq75Tofh.js";const t=i("PushNotifications",{});export{t as PushNotifications};
---
+++ server/web_dist/assets/index-DgWi47qo.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B7hap1x2.js","assets/index-Hq75Tofh.js"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-Hq75Tofh.js";const _=p("App",{web:()=>r(()=>import("./web-B7hap1x2.js"),__vite__mapDeps([0,1])).then(e=>new e.AppWeb)});export{_ as App};
