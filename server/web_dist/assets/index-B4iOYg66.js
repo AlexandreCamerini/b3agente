16f0f20fdb30bfedecc77328fb44f3a8871c1f1d
Alexandre Camerini <alexandre.camerini@gmail.com>
2026-08-24 00:37:57 -0300

    entrega

---
+++ server/web_dist/assets/index-B4iOYg66.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-7I0De256.js","assets/index-Dv_FaUn5.js"])))=>i.map(i=>d[i]);
+import{r as e,_ as o}from"./index-Dv_FaUn5.js";const _=e("Browser",{web:()=>o(()=>import("./web-7I0De256.js"),__vite__mapDeps([0,1])).then(r=>new r.BrowserWeb)});export{_ as Browser};
