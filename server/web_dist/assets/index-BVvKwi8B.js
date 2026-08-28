a17c25105b20ad91015ac0f7ac6c86529188cca1
Alexandre Camerini <alexandre.camerini@gmail.com>
2026-08-27 21:06:09 -0300

    chore(09-06): carimba e publica o front (F10-20260827-01)
    - scripts/bump.sh avança web/src/version.js de F10-20260824-01 para
      F10-20260827-01
    - scripts/publicar-web.sh builda e copia web/dist -> server/web_dist,
      sincronizando SERVER_BUILD_ID em server/app/main.py
    - suíte canônica (bash scripts/executar.sh --testes) verde após a
      publicação: backend + web/tests/*.mjs, incluindo os guardiões de
      asset/bundle publicado
    - nenhum segredo no bundle: grep -rc 'MYDATA_TOKEN' server/web_dist
      retorna 0 em todos os arquivos

--- server/web_dist/assets/index-BVvKwi8B.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DfYaS09D.js","assets/index-CSX7iKU8.js"])))=>i.map(i=>d[i]);
-import{r,_ as p}from"./index-CSX7iKU8.js";const _=r("AppLauncher",{web:()=>p(()=>import("./web-DfYaS09D.js"),__vite__mapDeps([0,1])).then(e=>new e.AppLauncherWeb)});export{_ as AppLauncher};
---
+++ server/web_dist/assets/index-CQUehCa2.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CNQCaZk1.js","assets/index-D56ZQIEE.js"])))=>i.map(i=>d[i]);
+import{r as e,_ as o}from"./index-D56ZQIEE.js";const _=e("Browser",{web:()=>o(()=>import("./web-CNQCaZk1.js"),__vite__mapDeps([0,1])).then(r=>new r.BrowserWeb)});export{_ as Browser};
