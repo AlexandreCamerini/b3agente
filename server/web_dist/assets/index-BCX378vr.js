245caccb47fcdee0ca3eb1580bb968eab179caa3
Alexandre Camerini <alexandre.camerini@gmail.com>
2026-09-13 00:56:05 -0300

    Evolucao do modulo opcoes

---
+++ server/web_dist/assets/index-BCX378vr.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-c9fa69sf.js","assets/index-DtIFpNt-.js"])))=>i.map(i=>d[i]);
+import{r,_ as p}from"./index-DtIFpNt-.js";const _=r("AppLauncher",{web:()=>p(()=>import("./web-c9fa69sf.js"),__vite__mapDeps([0,1])).then(e=>new e.AppLauncherWeb)});export{_ as AppLauncher};
--- server/web_dist/assets/index-C0MFsuUc.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Drxv5p00.js","assets/index-DfbxktjE.js"])))=>i.map(i=>d[i]);
-import{r as p,_ as r}from"./index-DfbxktjE.js";const _=p("App",{web:()=>r(()=>import("./web-Drxv5p00.js"),__vite__mapDeps([0,1])).then(e=>new e.AppWeb)});export{_ as App};
