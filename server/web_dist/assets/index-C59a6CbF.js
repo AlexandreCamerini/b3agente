3eb90879f20feaa268dc4a0e7304138fd089babb
Alexandre Camerini <alexandre.camerini@gmail.com>
2026-09-24 17:55:55 -0300

    Nova Aba Opcoes

---
+++ server/web_dist/assets/index-C59a6CbF.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DM9-vL1n.js","assets/index-CDk13Ywa.js"])))=>i.map(i=>d[i]);
+import{r,_ as p}from"./index-CDk13Ywa.js";const _=r("AppLauncher",{web:()=>p(()=>import("./web-DM9-vL1n.js"),__vite__mapDeps([0,1])).then(e=>new e.AppLauncherWeb)});export{_ as AppLauncher};
